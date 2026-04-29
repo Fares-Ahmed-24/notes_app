da9fd3ca03fae956fdbcb8e23370ded91d410f0a
Fares <fa6927351@gmail.com>
2026-04-29 15:18:25 +0300

    custome text field

---
+++ lib/widgets/custome_text_field.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
+
+class CustomeTextField extends StatelessWidget {
+  const CustomeTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+
+      decoration: InputDecoration(
+        hintText: 'Title',
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: BorderSide(color: color ?? Colors.white),
+  );
+}
