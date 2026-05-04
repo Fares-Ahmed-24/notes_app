fb9744ed0aa1bf1c028c7966a92abf42d3042dab
Fares <fa6927351@gmail.com>
2026-05-04 03:35:28 +0300

    init hive & open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/view/notes_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(NotesApp());
 }
 
@@ -12,10 +16,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: "Poppins",
-      ),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
       home: const NotesView(),
     );
   }
